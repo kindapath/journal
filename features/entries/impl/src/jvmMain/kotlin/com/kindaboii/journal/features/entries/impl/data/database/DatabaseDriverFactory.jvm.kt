f02649cc9c4e95850d2a913d6ac8a269d84d45f3
kindapath <kindaboii@yandex.ru>
2026-01-30 21:27:40 +0300

    feat: save db to home/documents folder;

--- features/entries/impl/src/jvmMain/kotlin/com/kindaboii/journal/features/entries/impl/data/database/DatabaseDriverFactory.jvm.kt
+++ features/entries/impl/src/jvmMain/kotlin/com/kindaboii/journal/features/entries/impl/data/database/DatabaseDriverFactory.jvm.kt
@@ -2,10 +2,27 @@ package com.kindaboii.journal.features.entries.impl.data.database
 
 import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.driver.jdbc.sqlite.JdbcSqliteDriver
+import java.io.File
 
 actual class DatabaseDriverFactory {
     actual suspend fun createDriver(): SqlDriver {
-        return JdbcSqliteDriver(JdbcSqliteDriver.IN_MEMORY)
-            .also { EntryDatabase.Schema.create(it).await() }
+        val appFolderName = "JournalApp"
+        val dbName = "entries.db"
+
+        val documentsFolder = File(System.getProperty("user.home"), "Documents")
+        val appFolder = File(documentsFolder, appFolderName)
+
+        if (!appFolder.exists()) {
+            appFolder.mkdirs()
+        }
+
+        val databaseFile = File(appFolder, dbName)
+        val driver: SqlDriver = JdbcSqliteDriver("jdbc:sqlite:${databaseFile.absolutePath}")
+
+        if (!databaseFile.exists() || databaseFile.length() == 0L) {
+            EntryDatabase.Schema.create(driver).await()
+        }
+
+        return driver
     }
 }
