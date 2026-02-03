9f0c4af73857dc91f10ecb3e239da9fcd023a69d
kindapath <kindaboii@yandex.ru>
2026-02-03 20:13:48 +0300

    refactor: change dto model;

--- features/entries/impl/src/commonMain/kotlin/com/kindaboii/journal/features/entries/impl/data/database/datasource/remote/models/EntryDto.kt
+++ features/entries/impl/src/commonMain/kotlin/com/kindaboii/journal/features/entries/impl/data/database/datasource/remote/models/EntryDto.kt
@@ -3,6 +3,7 @@ package com.kindaboii.journal.features.entries.impl.data.database.datasource.rem
 import com.kindaboii.journal.features.entries.api.models.Entry
 import com.kindaboii.journal.features.entries.api.models.Mood
 import kotlin.time.Instant
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
@@ -12,50 +13,54 @@ data class EntriesResponse(
 
 @Serializable
 data class EntryDto(
+
     val id: String,
     val title: String? = null,
     val body: String? = null,
-    val mood: MoodDto? = null,
-    val createdAt: String,
-    val updatedAt: String? = null,
-    val deletedAt: String? = null,
-)
 
-@Serializable
-data class MoodDto(
-    val value: Long,
-    val emotions: List<String> = emptyList(),
-    val influences: List<String> = emptyList(),
+    @SerialName("mood_value")
+    val moodValue: Long? = null,
+
+    @SerialName("mood_emotions")
+    val moodEmotions: List<String> = emptyList(),
+
+    @SerialName("mood_influences")
+    val moodInfluences: List<String> = emptyList(),
+
+    @SerialName("created_at")
+    val createdAt: String,
+
+    @SerialName("updated_at")
+    val updatedAt: String? = null,
+
+    @SerialName("deleted_at")
+    val deletedAt: String? = null,
 )
 
 fun EntryDto.toModel() = Entry(
     id = id,
     title = title,
     body = body,
-    mood = mood?.toModel(),
+    mood = moodValue?.let {
+        Mood(
+            value = it,
+            emotions = moodEmotions,
+            influences = moodInfluences,
+        )
+    },
     createdAt = Instant.parse(createdAt),
     updatedAt = updatedAt?.let { Instant.parse(it) },
     deletedAt = deletedAt?.let { Instant.parse(it) },
-)
-
-fun MoodDto.toModel() = Mood(
-    value = value,
-    emotions = emotions,
-    influences = influences,
 )
 
 fun Entry.toDto() = EntryDto(
     id = id,
     title = title,
     body = body,
-    mood = mood?.toDto(),
+    moodValue = mood?.value,
+    moodEmotions = mood?.emotions ?: emptyList(),
+    moodInfluences = mood?.influences ?: emptyList(),
     createdAt = createdAt.toString(),
     updatedAt = updatedAt?.toString(),
     deletedAt = deletedAt?.toString(),
-)
-
-fun Mood.toDto() = MoodDto(
-    value = value,
-    emotions = emotions,
-    influences = influences,
 )
