e79252cedc18e4538d344f2f57971d51e872d921
Eugene <kindaboii@yandex.ru>
2026-04-08 15:59:42 +0300

    chore: disable supabase logging;

--- common/network/src/commonMain/kotlin/com/kindaboii/journal/network/SupabaseClientFactory.kt
+++ common/network/src/commonMain/kotlin/com/kindaboii/journal/network/SupabaseClientFactory.kt
@@ -1,23 +1,32 @@
 package com.kindaboii.journal.network
 
 import io.github.jan.supabase.SupabaseClient
+import io.github.jan.supabase.annotations.SupabaseInternal
 import io.github.jan.supabase.auth.Auth
 import io.github.jan.supabase.createSupabaseClient
 import io.github.jan.supabase.logging.LogLevel
 import io.github.jan.supabase.postgrest.Postgrest
 import io.github.jan.supabase.realtime.Realtime
 
-fun createSupabaseClient(): SupabaseClient =
-    createSupabaseClient(
+fun createSupabaseClient(): SupabaseClient {
+    silenceSupabaseLogs()
+    return createSupabaseClient(
         supabaseUrl = ApiConfig.SUPABASE_URL,
         supabaseKey = ApiConfig.SUPABASE_CLIENT_API_KEY,
     ) {
-
         defaultLogLevel = LogLevel.NONE
         install(Auth)
         install(Postgrest) {
             defaultSchema = ApiConfig.SUPABASE_SCHEMA
         }
-        install(Realtime)
-
+        install(Realtime) 
     }
+}
+
+@OptIn(SupabaseInternal::class)
+private fun silenceSupabaseLogs() {
+    runCatching { SupabaseClient.LOGGER.setLevel(LogLevel.NONE) }
+    runCatching { Auth.logger.setLevel(LogLevel.NONE) }
+    runCatching { Postgrest.logger.setLevel(LogLevel.NONE) }
+    runCatching { Realtime.logger.setLevel(LogLevel.NONE) }
+}
